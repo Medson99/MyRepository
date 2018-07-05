190a0702864cc6d458b946863de7d789e478d781
Medson99 <40857608+Medson99@users.noreply.github.com>
2018-07-05 13:59:26 +0300

    Test_Task
    Test_Task

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Тестовое задание для Junior .NET developer  
+//Гурин Вадим, e-mail: herison777@gmail.com
+namespace TestConsole
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			bool enterCheck = false;
+			do
+			{
+				Console.Clear();
+				do
+				{
+					try
+					{
+						Console.Write("Введите индекс начала последовательности: ");
+						var begin = int.TryParse(Console.ReadLine(), out var res) ? res : 0;
+						Console.Write("Введите индекс конца последовательности: ");
+						var end = int.TryParse(Console.ReadLine(), out var result) ? result : 0;
+						(begin, end).ToTuple().GetPart().ForEach(num => Console.Write($"{num}; "));
+
+						if (end > begin)
+						{
+							enterCheck = true;
+						}
+					}
+					catch (Exception)
+					{
+						Console.WriteLine("Введены не верные данные, попробуйте ещё раз!");
+						Console.ReadKey();
+						Console.Clear();
+						enterCheck = false;
+					}
+				} while (!enterCheck);
+
+				Console.WriteLine("\nНажмите <Esc> для выхода или любую клавижу для продолжения");
+			} while (Console.ReadKey().Key != ConsoleKey.Escape);
+		}
+
+		private static List<long> GetPart(this Tuple<int, int> bounds)
+		{
+			var initList = new List<long> { 1, 1, 1 };
+
+			for (var i = 0; i < bounds.Item2 - 3; i++)
+			{
+				initList.Add(initList.Skip(Math.Max(0, initList.Count - 3)).Sum());
+			}
+
+			return initList.GetRange(bounds.Item1 - 1, bounds.Item2 - bounds.Item1 + 1).ToList();
+		}
+	}
+}
